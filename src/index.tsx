567483171e9fdebff86b424ed6ab55c86a4be638
Seong <pok_gare@naver.com>
2020-04-29 18:44:15 +0900

    리덕스 thunk 적용

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './modules';
+import ReduxThunk from 'redux-thunk';
 
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(ReduxThunk, logger))
+);
 
 ReactDOM.render(
   <Provider store={store}>
